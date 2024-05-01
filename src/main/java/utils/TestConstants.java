e086e51f486fe48830bf571080a410b75cd5a721
magdalsen <magdal.sen@gmail.com>
2024-05-01 17:00:24 +0200

    update test with data provider and testconstants file

---
+++ src/main/java/utils/TestConstants.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class TestConstants {
+    public static final String BASE_URL = "https://api.github.com";
+    public static final String EXPECTED_COMPANY_NAME = "@github";
+    public static final String EXPECTED_LOCATION = "San Francisco";
+}
--- src/test/java/tests/GitHubApiTest.java
+++ src/test/java/tests/GitHubApiTest.java
@@ -3,35 +3,43 @@ package tests;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import static utils.TestConstants.*;
+
 
 public class GitHubApiTest {
     private GitHubUserPage gitHubUserPage;
-    private String baseUrl = "https://api.github.com";
-    private String username = "octocat";
     
     @BeforeClass
     public void setUp() {
-        gitHubUserPage = new GitHubUserPage(baseUrl);
+        gitHubUserPage = new GitHubUserPage(BASE_URL);
     }
     
-    @Test
-    public void testGetUserDetails() {
+    @Test(dataProvider = "usernames")
+    public void testGetUserDetails(String username, String expectedName) {
         Response response = gitHubUserPage.getUserDetails(username);
         
         // Asercje
         Assert.assertEquals(response.statusCode(), 200, "Status code should be 200 OK");
         
         String name = response.jsonPath().getString("name");
-        Assert.assertEquals(name, "The Octocat", "Incorrect user name");
+        Assert.assertEquals(name, expectedName, "Incorrect user name");
         
         String company = response.jsonPath().getString("company");
-        Assert.assertEquals(company, "@github", "Incorrect company name");
+        Assert.assertEquals(company, EXPECTED_COMPANY_NAME, "Incorrect company name");
         
         String location = response.jsonPath().getString("location");
-        Assert.assertEquals(location, "San Francisco", "Incorrect location");
+        Assert.assertEquals(location, EXPECTED_LOCATION, "Incorrect location");
         
         int followers = response.jsonPath().getInt("followers");
         Assert.assertTrue(followers > 0, "Followers count should be greater than 0");
+    }
+    
+    @DataProvider(name = "usernames")
+    public Object[][] getUsernames() {
+        return new Object[][] {
+            {"octocat", "The Octocat"},
+        };
     }
 }
